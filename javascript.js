deac5fc25d3938032d268809dbb6ce2d1adf5b5a
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-03-22 10:19:20 +0100

    második verzió

--- javascript.js
+++ javascript.js
@@ -1,84 +1,85 @@
 window.addEventListener("load", init);
-window.addEventListener("click", galeria);
 
 
 function ID(elem) {
     return document.getElementById(elem);
 }
+function CLASS(elem) {
+    return document.getElementsByClassName(elem);
+}
+
 
 var kepeim = [{
     eleres: "kepek/kep01.jpg",
     cim: "Állatok",
-    leírás: "Kecskék",},
+    leiras: "Kecskék",},
     {
     eleres: "kepek/kep02.jpg",
     cim: "Japán fa",
-    leírás: "Szép sétány"
+    leiras: "Szép sétány"
     },
     {
     eleres: "kepek/kep03.jpg",
     cim: "Sárga virágok",
-    leírás: "Szép mezőföld"
+    leiras: "Szép mezőföld"
     },
     {
     eleres: "kepek/kep04.jpg",
     cim: "Tájkép",
-    leírás: "Fent a magasban!"
+    leiras: "Fent a magasban!"
     },
     {
     eleres: "kepek/kep05.jpg",
     cim: "Az út mellet",
-    leírás: "Szép fehér virágok az út mellet"
+    leiras: "Szép fehér virágok az út mellet"
     }]
 
 var kepindex = 0;
 function galeria(){
-    if (kepindex == 0){
-        var txt ="<div class='galeria' id='elsokep'><img src='"+kepeim[4].eleres+"' alt='"+kepeim[4].leírás+"' id='kivalasztott' onclick='elsokepClick()'></div><div class='galeria' id='masodikkep'><img src='"+kepeim[kepindex].eleres+"' alt='"+kepeim[kepindex].leírás+"' id='kivalasztott'></div><div class='galeria' id='harmadikkep'><img src='"+kepeim[kepindex+1].eleres+"'' alt='"+kepeim[kepindex+1].leírás+"' id='kivalasztott' onclick='harkepClick()'></div>";
-    }else if (kepindex == 4){
-    var txt ="<div class='galeria'  id='elsokep'><img src='"+kepeim[kepindex-1].eleres+"' alt='"+kepeim[kepindex-1].leírás+"' id='kivalasztott' onclick='elsokepClick()'></div><div class='galeria' id='masodikkep'><img src='"+kepeim[kepindex].eleres+"' alt='"+kepeim[kepindex].leírás+"' '></div><div class='galeria' id='harmadikkep'><img src='"+kepeim[0].eleres+"'' alt='"+kepeim[0].leírás+"' id='kivalasztott' onclick='harkepClick()'></div>";
-    }else{
-        var txt ="<div class='galeria' id='elsokep'><img src='"+kepeim[kepindex-1].eleres+"' alt='"+kepeim[kepindex-1].leírás+"' '></div><div class='galeria' id='masodikkep'><img src='"+kepeim[kepindex].eleres+"' alt='"+kepeim[kepindex].leírás+"' id='kivalasztott' onclick='elsokepClick()'></div><div class='galeria' id='harmadikkep'><img src='"+kepeim[kepindex+1].eleres+"'' alt='"+kepeim[kepindex+1].leírás+"' id='kivalasztott' onclick='harkepClick()'></div>";
+    var txt="";
+    for (let i = 0; i < kepeim.length; i++) {
+        txt+=
+        `<div class="kep">
+            <img id="${i}" src="${kepeim[i].eleres}" alt="${kepeim[i].cim}">
+        </div>`;
     }
     ID("inp").innerHTML=txt;
+    for (let index = 0; index < kepeim.length; index++) {
+        ID(index).addEventListener("click", kepKivalasztas)
+        CLASS("kep")[index].style.border="5px solid red";
+        CLASS("kep")[index].className+=" kepekformazasa";
+    }
 }
+
+function kepKivalasztas() {
+    console.log(event.target.id);
+    kepindex=Number(event.target.id);
+    megjelenit();
+}
+
 function init() {
+    megjelenit();
+    galeria();
     ID("balra").addEventListener("click", balra);
     ID("jobbra").addEventListener("click", jobbra);
 }
-function megjelenit(kepindex) {
+function megjelenit() {
     ID("kivalasztott").src = kepeim[kepindex].eleres;
     ID("kivalasztott").alt = kepeim[kepindex].cim;
-
 }
 function balra() {
+    kepindex --;
     if(kepindex <= 0){
         kepindex = kepeim.length-1;
-    }
-    else{
-        kepindex -= 1;
     }
     console.log(kepindex);
     megjelenit(kepindex);
 }
 function jobbra() {
+    kepindex ++;
     if(kepindex >= kepeim.length-1){
         kepindex = 0;
     }
-    else{
-        kepindex += 1;
-    }
     console.log(kepindex);
     megjelenit(kepindex)
-}
-
-function elsokepClick(){
-    let text = document.getElementById("elsokep").innerHTML;
-    document.getElementById("valtoz").innerHTML = text;
-    console.log(text)
-}
-function harkepClick(){
-    let text = document.getElementById("harmadikkep").innerHTML;
-    document.getElementById("valtoz").innerHTML = text;
-    console.log(text)
 }
